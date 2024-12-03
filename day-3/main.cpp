d4357fadbe5d271f2135018d37cd0b378bd50799
Elias Lehtinen <elias.lehtinen01@gmail.com>
2024-12-03 14:23:12 +0200

    Day three second star

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -5,26 +5,35 @@
 
 
 constexpr char newline = '\n';
+const std::string do_string = "do()";
+const std::string dont_string = "don't()";
+const std::regex mul { R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))" };
 
 
-void first(std::vector<std::string> lines) {
-  const std::regex mul { R"(mul\((\d+),(\d+)\))" };
+int first(std::smatch match) {
+  if (match[0] == do_string || match[0] == dont_string) {
+    return 0;
+  }
 
-  int result = 0;
-
-  std::ranges::for_each(lines, [mul, &result](std::string line) {
-    auto begin = std::sregex_iterator(line.begin(), line.end(), mul);
-    auto end = std::sregex_iterator {};
-    for (auto iterator = begin; iterator != end; ++iterator) {
-      std::smatch match = *iterator;
-      result += std::stoi(match[1]) * std::stoi(match[2]);
-    }
-  });
-
-  std::cout << result << newline;
+  return std::stoi(match[1]) * std::stoi(match[2]);
 }
 
-void second(std::vector<std::vector<int>> reports) {
+int second(std::smatch match, bool& dont) {
+  if (match[0] == do_string) {
+    dont = false;
+    return 0;
+  }
+
+  if (match[0] == dont_string) {
+    dont = true;
+    return 0;
+  }
+
+  if (!dont) {
+    return std::stoi(match[1]) * std::stoi(match[2]);
+  }
+
+  return 0;
 }
 
 int main() {
@@ -34,5 +43,21 @@ int main() {
     lines.push_back(line);
   }
 
-  first(lines);
+
+  int first_result = 0;
+  int second_result = 0;
+  bool dont = false;
+
+  std::ranges::for_each(lines,
+      [&first_result, &second_result, &dont](std::string line) {
+        auto begin = std::sregex_iterator(line.begin(), line.end(), mul);
+        std::for_each(begin, std::sregex_iterator {},
+            [&first_result, &second_result, &dont](std::smatch match) {
+              first_result += first(match);
+              second_result += second(match, dont);
+            });
+      });
+
+  std::cout << first_result << newline;
+  std::cout << second_result << newline;
 }
